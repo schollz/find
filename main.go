f362d7b15031c863b97663e9776199a2ea983215
Zack Scholl <zack.scholl@gmail.com>
2016-04-17 11:46:24 -0400

    Much improved with better logging

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"os/exec"
@@ -15,6 +14,7 @@ import (
 	"time"
 
 	"github.com/codegangsta/cli"
+	"github.com/op/go-logging"
 )
 
 // Fingerprint to be sent to the server
@@ -31,6 +31,20 @@ type WifiData struct {
 	Mac  string `json:"mac"`
 	Rssi int    `json:"rssi"`
 }
+
+var log = logging.MustGetLogger("example")
+
+// Example format string. Everything except the message has a custom color
+// which is dependent on the log level. Many fields have a custom output
+// formatting too, eg. the time returns the hour down to the milli second.
+var format1 = logging.MustStringFormatter(
+	`%{time:15:04:05.000} %{message}`,
+)
+var format2 = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} - %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+var verbose = true
 
 func getInput(prompt string) string {
 	reader := bufio.NewReader(os.Stdin)
@@ -70,9 +84,42 @@ func scanWifi() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	log.Println("Gathering fingerprint with '" + command + "'")
+	log.Info("Gathering fingerprint with '" + command + "'")
 	out, err := exec.Command(strings.Split(command, " ")[0], strings.Split(command, " ")[1:]...).Output()
 	return string(out), err
+}
+
+func setupLogging() {
+	// For demo purposes, create two backend for os.Stderr.
+	backend1 := logging.NewLogBackend(os.Stdout, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+
+	// For messages written to backend2 we want to add some additional
+	// information to the output, including the used log level and the name of
+	// the function.
+	backend1Formatter := logging.NewBackendFormatter(backend1, format1)
+	backend2Formatter := logging.NewBackendFormatter(backend2, format2)
+
+	// Only errors and more severe messages should be sent to backend1
+	backend1Leveled := logging.AddModuleLevel(backend1Formatter)
+	backend1Leveled.SetLevel(logging.CRITICAL, "")
+	// Everything should be sent to backend2
+	backend2Leveled := logging.AddModuleLevel(backend2Formatter)
+	backend2Leveled.SetLevel(logging.INFO, "")
+
+	// Set the backends to be used.
+	if verbose {
+		logging.SetBackend(backend2Leveled)
+	} else {
+		logging.SetBackend(backend1Leveled)
+	}
+
+	// log.Info("debug")
+	// log.Info("info")
+	// log.Notice("notice")
+	// log.Warning("warning")
+	// log.Error("err")
+	// log.Critical("crit")
 }
 
 func main() {
@@ -116,6 +163,10 @@ func main() {
 			Name:  "learn,e",
 			Usage: "need to set if you want to learn location",
 		},
+		cli.BoolFlag{
+			Name:  "nodebug,d",
+			Usage: "turns off debugging",
+		},
 	}
 	app.Action = func(c *cli.Context) {
 		times = c.Int("continue")
@@ -123,13 +174,13 @@ func main() {
 		f.Group = strings.ToLower(c.String("group"))
 		if f.Group == "group" {
 			f.Group = getInput("Enter group: ")
-			fmt.Println("Next time use './fingerprint -g " + f.Group + "'")
+			fmt.Println("Instead of typing next time, add '-g " + f.Group + "'")
 		}
 		// make sure to get a location if learning
 		f.Location = strings.ToLower(c.String("location"))
 		if c.Bool("learn") && c.String("location") == "location" {
 			f.Location = getInput("Enter location: ")
-			fmt.Println("Next time use './fingerprint -g " + f.Group + " -l " + f.Location + "'")
+			fmt.Println("Instead of typing next time, add '-l " + f.Location + "'")
 		}
 		// set server
 		if c.Bool("learn") {
@@ -140,20 +191,24 @@ func main() {
 		// set fingerprint things
 		f.Time = time.Now().UnixNano() / 1000000
 		f.Username = strings.ToLower(c.String("user"))
+		verbose = !c.Bool("debug")
+		setupLogging()
 	}
 	app.Run(os.Args)
 
 	// Print the current parameters
-	fmt.Println("User: " + f.Username)
-	fmt.Println("Group: " + f.Group)
-	fmt.Println("Server: " + address)
+	log.Notice("You can see fewer messages by adding --nodebug")
+	log.Notice("User: " + f.Username)
+	log.Notice("Group: " + f.Group)
+	log.Notice("Server: " + address)
 	if strings.Contains(address, "/learn") {
-		fmt.Println("Location: " + f.Location)
+		log.Notice("Location: " + f.Location)
 	}
+	log.Notice("Running", times, "times (you can run more using '-c SOMENUM'). Please wait...")
 
 	for i := 0; i < times; i++ {
 
-		log.Println("Scanning Wifi")
+		log.Info("Scanning Wifi")
 		out, err := scanWifi()
 		if err != nil {
 			if strings.Contains(err.Error(), "255") {
@@ -163,18 +218,18 @@ func main() {
 			log.Fatal(string(out), err)
 		}
 
-		log.Println("Processing ", len(strings.Split(out, "\n")), " lines out output")
+		log.Info("Processing", len(strings.Split(out, "\n")), "lines out output")
 		f.WifiFingerprint, err = processOutput(out, runtime.GOOS)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		log.Println("Sending fingerprint to " + address)
+		log.Info("Sending fingerprint to " + address)
 		response, err := sendFingerprint(address, f)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println("Response: " + response)
+		log.Info("Response: " + response)
 
 	}
 
@@ -198,6 +253,20 @@ func sendFingerprint(address string, f Fingerprint) (string, error) {
 	if strings.Contains(string(body), `"success":true`) == false && strings.Contains(string(body), `"success"`) == true {
 		return "", fmt.Errorf("Something wrong with server")
 	}
+	log.Info(string(body))
 
-	return string(body), nil
+	type Response struct {
+		Message string `json:"message"`
+		Success bool   `json:"success"`
+	}
+	var r Response
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		return "", err
+	}
+
+	if strings.Contains(r.Message, ":") {
+		log.Critical(strings.TrimSpace(strings.Split(r.Message, ":")[1]))
+	}
+	return r.Message, nil
 }
