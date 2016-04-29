f118665409cfc45fee01bd505862b9e16ebe9ae0
Zack Scholl <zack.scholl@gmail.com>
2016-04-28 22:10:17 -0400

    Added MQTT listening

--- mqtt.go
+++ mqtt.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 	"strings"
 	"time"
 
@@ -11,7 +13,28 @@ import (
 var mqttClients map[string]*MQTT.Client
 
 func init() {
-	go clearMqttPool()
+	if RuntimeArgs.Mqtt {
+		go clearMqttPool()
+
+		server := "tcp://ml.internalpositioning.com:1883"
+		name := "user" + strconv.Itoa(rand.Intn(1000))
+
+		subTopic := strings.Join([]string{"/find/", "fingerprint", "/+"}, "")
+		opts := MQTT.NewClientOptions().AddBroker(server).SetClientID(name).SetCleanSession(true)
+
+		opts.OnConnect = func(c *MQTT.Client) {
+			if token := c.Subscribe(subTopic, 1, messageReceived); token.Wait() && token.Error() != nil {
+				panic(token.Error())
+			}
+		}
+
+		client := MQTT.NewClient(opts)
+
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+		fmt.Printf("Connected to ", name, server)
+	}
 }
 
 func clearMqttPool() {
@@ -40,4 +63,10 @@ func sendMQTTMessage(message string, group string, user string) error {
 		return fmt.Errorf("Failed to send message")
 	}
 	return nil
+}
+
+func messageReceived(client *MQTT.Client, msg MQTT.Message) {
+	topics := strings.Split(msg.Topic(), "/")
+	msgFrom := topics[len(topics)-1]
+	fmt.Print(msgFrom + ": " + string(msg.Payload()))
 }
