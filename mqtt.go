d16f10c74c255cfa0a6b69e214316eb971386970
Zack Scholl <zack.scholl@gmail.com>
2016-04-28 21:12:11 -0400

    Added client pool

--- mqtt.go
+++ mqtt.go
@@ -3,9 +3,23 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
 )
+
+var mqttClients map[string]*MQTT.Client
+
+func init() {
+	go clearMqttPool()
+}
+
+func clearMqttPool() {
+	for {
+		mqttClients = make(map[string]*MQTT.Client)
+		time.Sleep(time.Minute * 30)
+	}
+}
 
 func sendMQTTMessage(message string, group string, user string) error {
 	server := "tcp://ml.internalpositioning.com:1883"
@@ -14,22 +28,15 @@ func sendMQTTMessage(message string, group string, user string) error {
 	// subTopic := strings.Join([]string{"/find/", room, "/+"}, "")
 	pubTopic := strings.Join([]string{"/find/", room, "/", name}, "")
 
-	opts := MQTT.NewClientOptions().AddBroker(server).SetClientID(name).SetCleanSession(true)
-
-	// opts.OnConnect = func(c *MQTT.Client) {
-	// 	if token := c.Subscribe(subTopic, 1, messageReceived); token.Wait() && token.Error() != nil {
-	// 		panic(token.Error())
-	// 	}
-	// }
-
-	client := MQTT.NewClient(opts)
-
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
+	if _, ok := mqttClients[group]; !ok {
+		opts := MQTT.NewClientOptions().AddBroker(server).SetClientID(name).SetCleanSession(true)
+		mqttClients[group] = MQTT.NewClient(opts)
+		if token := mqttClients[group].Connect(); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
 	}
-	// Debug.Println("Connected ", name, server)
 
-	if token := client.Publish(pubTopic, 1, false, message); token.Wait() && token.Error() != nil {
+	if token := mqttClients[group].Publish(pubTopic, 1, false, message); token.Wait() && token.Error() != nil {
 		return fmt.Errorf("Failed to send message")
 	}
 	return nil
