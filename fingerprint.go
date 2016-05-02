bc3731bf672888408170cf05af556340a03150f0
Zack Scholl <zack.scholl@gmail.com>
2016-05-02 15:14:28 -0400

    Fixed bug

--- fingerprint.go
+++ fingerprint.go
@@ -214,5 +214,5 @@ func trackFingerprint(jsonFingerprint Fingerprint) (string, bool, string, map[st
 		})
 		go sendMQTTLocation(string(mqttMessage), jsonFingerprint.Group, jsonFingerprint.Username)
 	}
-	return "Calculated location: " + locationGuess + " (" + strconv.Itoa(int(math.Exp(bayes[locationGuess]))) + "%)", true, locationGuess, bayes
+	return "Calculated location: " + locationGuess + " (" + strconv.Itoa(int(100*math.Exp(bayes[locationGuess]))) + "%)", true, locationGuess, bayes
 }
